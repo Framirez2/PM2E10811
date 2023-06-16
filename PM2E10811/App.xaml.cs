deb37add5e9f6186672ebcf4c7482f109a79b2b3
Framirez2 <116310432+Framirez2@users.noreply.github.com>
2023-06-15 19:54:23 -0600

    Agregar archivos de proyecto.

---
+++ PM2E10811/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PM2E10811.Views;
+using PM2E10811.Controllers;
+using PM2E10811.Models;
+using System.IO;
+
+namespace PM2E10811
+{
+    public partial class App : Application
+    {
+
+        static dbsitios basedatos;
+
+        public static dbsitios BaseDatos
+        {
+            get
+            {
+                if (basedatos == null)
+                {
+                    basedatos = new dbsitios(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SitiosDB.db3"));
+                }
+                return basedatos;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PM2E10811/Views/listarsitios.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PM2E10811.Views;
+using PM2E10811.Models;
+
+namespace PM2E10811.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    
+    public partial class listarsitios : ContentPage
+    {
+        private sitios sitio;
+
+        public listarsitios()
+        {
+            InitializeComponent();
+        }
+
+        private void toolmenu_Clicked(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void liestasistios_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+           sitio = (sitios)e.Item;
+           
+        }
+      
+        private async void btnvermapa_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new ver_mapa(sitio.latitud, sitio.longitud, sitio.descripcion));
+            }
+             catch
+            {
+                await DisplayAlert("Advertencia", "Favor seleccione el sitio donde desea ver en el mapa", "Ok");
+            }
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listasitios.ItemsSource = await App.BaseDatos.ObtenerlistadoSitio();
+        }
+
+
+        private async void btneliminacasa_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var eliminar = await App.BaseDatos.eliminarsitio(sitio);
+
+
+                if (eliminar != 0)
+                {
+                    await DisplayAlert("Advertencia", "Sitio eliminado con exito", "Aceptar");
+                    listasitios.ItemsSource = await App.BaseDatos.ObtenerlistadoSitio();
+
+                }
+                else
+                {
+                    await DisplayAlert("Advertencia", "Ha ocurrido un error", "Aceptar");
+                }
+            }
+            catch
+            {
+                await DisplayAlert("Advertencia", "Favor seleccione que sitio desea eliminar", "Aceptar");
+            }
+            
+
+            
+        }
+    }
+  }
